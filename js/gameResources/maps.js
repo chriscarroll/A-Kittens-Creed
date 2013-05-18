5414e2aeca8a00ebddc43f91041dd9fc4f5b8cc8
chriscarroll <chris.carroll1@gmail.com>
2013-05-18 18:05:45 +0100

    fecking merge

--- js/gameResources/maps.js
+++ js/gameResources/maps.js
@@ -12,12 +12,8 @@ gameResources.addMap = function addMap(name, source){
 
 gameResources.addMap("egypt","data/Renaisannce.tmx");
 gameResources.addMap("renaisannce","data/Renaisannce.tmx");
-<<<<<<< HEAD
 gameResources.addMap("spacemap","data/spacemap.tmx");
 gameResources.addMap("level2","data/level2.tmx");
-=======
-//gameResources.addMap("spacemap","data/spacemap.tmx");
->>>>>>> c3dfd052a734a04c276763fd48caf56cf73862db
 gameResources.addMap("level02","data/level02.tmx");
 gameResources.addMap("level3","data/level3.tmx");
 gameResources.addMap("level4","data/level4.tmx");
