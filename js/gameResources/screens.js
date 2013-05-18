a4d631d4d9c3769891d5c0576d13df497a71c69b
Simon Wielens <simonwielens@gmail.com>
2013-05-18 19:33:56 +0100

    added andy's comic

--- js/gameResources/screens.js
+++ js/gameResources/screens.js
@@ -11,3 +11,6 @@ gameResources.addScreenGraphics = function addScreenGraphics(name, source){
 gameResources.addScreenGraphics("titleScreen","data/GUI/titleScreen.png");
 gameResources.addScreenGraphics("storyScreen","data/GUI/scroll.png");
 gameResources.addScreenGraphics("gameover","data/GUI/endGame.png");
+gameResources.addScreenGraphics("comic1","data/introcomic1.jpg");
+gameResources.addScreenGraphics("comic2","data/introcomic2.jpg");
+gameResources.addScreenGraphics("comic3","data/intro comic3.jpg");
