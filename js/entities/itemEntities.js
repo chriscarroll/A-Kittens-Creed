ddf1ffa19e2ff89dd3af9a3429ccc8c37699e89a
Simon Wielens <simonwielens@gmail.com>
2013-05-18 20:52:44 +0100

    ffs

--- js/entities/itemEntities.js
+++ js/entities/itemEntities.js
@@ -347,6 +347,7 @@ var moveEntityXYAxis = entity("MoveEntityXYAxis",  me.ObjectEntity.extend({
     onCollision: function(res, obj) {
         if(res.obj.type == "Mirror"){
             this.vel.x = 0;
+
             this.vel.y = -3;
         }
        // me.game.remove(this);
